a126d0e66fef4bf29bb44e78344b949a8792fd10
EdL3R <96792899+Edl3R@users.noreply.github.com>
2024-08-27 19:01:32 +0300

    Update transport_catalogue.h for sprint 9

--- sprint8/transport_catalogue.h
+++ sprint8/transport_catalogue.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <string>
-#include <string_view>
 #include <unordered_map>
 #include <vector>
 #include <optional>
 #include <unordered_set>
+#include <string_view>
 #include "geo.h"
 
+namespace transport_catalogue {
 // Структура для представления остановки
 struct Stop {
     std::string name;
@@ -17,41 +18,49 @@ struct Stop {
 // Структура для представления маршрута
 struct Bus {
     std::string name;
-    std::vector<const Stop*> stops;
+    std::vector<std::string> stops;
     bool is_circular = false;       // Является ли маршрут кольцевым
-};
-
-// Структура для хранения информации о маршруте
-struct BusInfo {
-    size_t stop_count;
-    size_t unique_stop_count;
-    double route_length;
 };
 
 // Класс транспортного каталога
 class TransportCatalogue {
 public:
-    void AddStop(const std::string& name, Coordinates coordinates);
+    void AddStop(const Stop& stop);
 
-    void AddBus(const std::string& name, const std::vector<std::string>& stop_names, bool is_circular);
+    void AddBus(const Bus& bus);
 
-    const Stop* FindStop(const std::string& name) const;
+    void SetDistance(const std::string& from, const std::string& to, int distance);
+
+    const Stop* FindStop(const std::string_view& name) const;
 
     const Bus* FindBus(const std::string& name) const;
 
-    std::optional<BusInfo> GetBusInfo(const std::string& bus_name) const;
+    const std::vector<std::string>& GetBusStops(const std::string& bus_name) const;
 
-    std::optional<std::vector<std::string>> GetBusesByStop(const std::string& stop_name) const;
+    std::vector<std::string> GetBusesByStop(const std::string& stop_name) const;
+
+    std::tuple<int, int, double, double> GetBusInfo(const std::string& bus_name) const;
+
+    int GetDistance(const std::string& from, const std::string& to) const;
 
 private:
+
+    // Хеш-функция для пары указателей на остановки
+    struct PairHash {
+        size_t operator()(const std::pair<const Stop*, const Stop*>& pair) const {
+            return std::hash<const void*>()(pair.first) ^ std::hash<const void*>()(pair.second);
+        }
+    };
     // Контейнеры для хранения информации об остановках и маршрутах
     std::unordered_map<std::string, Stop> stops_;
     std::unordered_map<std::string, Bus> buses_;
 
     // Карты для быстрого поиска остановок и маршрутов по их названиям
-    std::unordered_map<std::string, const Stop*> stop_name_to_stop_;
-    std::unordered_map<std::string, const Bus*> bus_name_to_bus_;
+    std::vector<std::string> stop_names_;
 
-    // Обратная связь между остановками и маршрутами
-    std::unordered_map<std::string, std::unordered_set<std::string>> stop_to_buses_;
+    std::unordered_map<std::string_view, std::unordered_set<std::string>> buses_by_stop_;
+
+    // Хранение расстояниz
+    std::unordered_map<std::pair<const Stop*, const Stop*>, double, PairHash> distances_;
 };
+}
