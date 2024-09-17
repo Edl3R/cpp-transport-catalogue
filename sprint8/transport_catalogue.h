07ac3925fec33348e35e6e21f3bbb4c77a45d146
EdL3R <96792899+Edl3R@users.noreply.github.com>
2024-09-17 19:12:49 +0300

    Update transport_catalogue.h

--- sprint8/transport_catalogue.h
+++ sprint8/transport_catalogue.h
@@ -3,64 +3,54 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
-#include <optional>
 #include <unordered_set>
 #include <string_view>
-#include "geo.h"
+#include <optional>
+#include "domain.h"
 
 namespace transport_catalogue {
-// Структура для представления остановки
-struct Stop {
-    std::string name;
-    Coordinates coordinates;
-};
 
-// Структура для представления маршрута
-struct Bus {
-    std::string name;
-    std::vector<const Stop*> stops;
-    bool is_circular = false;       // Является ли маршрут кольцевым
-};
+    class TransportCatalogue {
+    public:
+        void AddStop(const domain::Stop& stop);
 
-// Класс транспортного каталога
-class TransportCatalogue {
-public:
-    void AddStop(const std::string& name, Coordinates coordinates);
+        void AddBus(const domain::Bus& bus);
 
-    void AddBus(const std::string& name, const std::vector<std::string>& stop_names, bool is_circular);
+        void SetDistance(const std::string_view& from, const std::string_view& to, int distance);
 
-    void SetDistance(const std::string& from, const std::string& to, int distance);
+        const domain::Stop* FindStop(const std::string_view& name) const;
 
-    const Stop* FindStop(const std::string_view& name) const;
+        const domain::Bus* FindBus(const std::string_view& name) const;
 
-    const Bus* FindBus(const std::string_view& name) const;
+        const std::vector<std::string_view>& GetBusStops(const std::string_view& bus_name) const;
 
-    const std::vector<const Stop*>& GetBusStops(const std::string& bus_name) const;
+        domain::BusInfo GetBusInfo(const std::string_view& bus_name) const;
 
-    std::vector<std::string> GetBusesByStop(const std::string& stop_name) const;
+        std::optional<std::vector<std::string>> GetBusesByStop(const std::string_view& stop_name) const;
 
-    std::tuple<int, int, double, double> GetBusInfo(const std::string& bus_name) const;
+        int GetDistance(const std::string_view& from, const std::string_view& to) const;
 
-    int GetDistance(const std::string& from, const std::string& to) const;
+        const std::unordered_map<std::string, domain::Bus>& GetBuses() const;
+        const std::unordered_map<std::string, domain::Stop>& GetStops() const;
+        void UpdateFilteredStops(const std::unordered_set<std::string>& stops_in_routes);
 
-private:
 
-    // Хеш-функция для пары указателей на остановки
-    struct PairHash {
-        size_t operator()(const std::pair<const Stop*, const Stop*>& pair) const {
-            return std::hash<const void*>()(pair.first) ^ std::hash<const void*>()(pair.second);
-        }
+    private:
+        struct PairHash {
+            size_t operator()(const std::pair<const domain::Stop*, const domain::Stop*>& pair) const;
+        };
+        // Контейнеры для хранения информации об остановках и маршрутах
+        std::unordered_map<std::string, domain::Stop> stops_;
+        std::unordered_map<std::string, domain::Bus> buses_;
+
+
+        std::unordered_map<std::string_view, std::unordered_set<std::string>> buses_by_stop_;
+
+        // Хранение расстояниz
+        std::unordered_map<std::pair<const domain::Stop*, const domain::Stop*>, int, PairHash> distances_;
+
+        std::vector<std::string_view> stop_string_views_;
+        std::unordered_map<std::string, domain::Stop> filtered_stops_;
     };
-    // Контейнеры для хранения информации об остановках и маршрутах
-    std::unordered_map<std::string, Stop> stops_;
-    std::unordered_map<std::string, Bus> buses_;
 
-    // Карты для быстрого поиска остановок и маршрутов по их названиям
-    std::vector<std::string> stop_names_;
-
-    std::unordered_map<std::string_view, std::unordered_set<std::string>> buses_by_stop_;
-
-    // Хранение расстояниz
-    std::unordered_map<std::pair<const Stop*, const Stop*>, double, PairHash> distances_;
-};
-}
+}  // namespace transport_catalogue
